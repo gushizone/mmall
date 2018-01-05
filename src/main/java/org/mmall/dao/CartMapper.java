a1846fe03c1a5df28dcdf28c8a8577cbe02b2fdd
gushizone <gushizone@gmail.com>
2018-01-05 14:26:29 +0800

    project init commit

---
+++ src/main/java/org/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package org.mmall.dao;
+
+import org.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/org/mmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package org.mmall.dao;
+
+import org.mmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
